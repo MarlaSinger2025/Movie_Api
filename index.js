7eb07421ffffb7b12fa641fe54161407881a50e6
MarlaSinger2025 <Marla_Singer_2020@web.de>
2025-12-12 15:23:26 +0100

    UpdatedIndex

--- index.js
+++ index.js
@@ -3,193 +3,207 @@ const express = require('express'),
     uuid = require('uuid'),
     bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const e = require('express');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/cfDB');
 
 app.use(bodyParser.json());
 
-let movies = [
-  {
-    "Title": "Amelie",
-    "OriginalTitle": "Le Fabuleux Destin d'Amélie Poulain",
-    "Description": "(Amelie, 2001). At a tiny Parisian café, the adorable yet painfully shy Amélie accidentally discovers a gift for helping others. Soon Amelie is spending her days as a matchmaker, guardian angel, and all-around do-gooder. But when she bumps into a handsome stranger, will she find the courage to become the star of her very own love story?",
-    "Genre": {
-        "Name": "Romantic Comedy",
-        "Description": "Romantic comedy (commonly shortened to romcom or rom-com) is a sub-genre of comedy and romance fiction, focusing on lighthearted, humorous plot lines centered on romantic ideas, such as how love is able to surmount all obstacles"
-    },
-    "Director":{
-        "Name": "Jean-Pierre Jeunet",
-        "Bio": "Jean-Pierre Jeunet is a French film director and screenwriter known for the films Delicatessen, The City of Lost Children, Alien: Resurrection and Amélie.",
-        "Birthyear": "1953"
-    },
-    "imageURL": "https://media.themoviedb.org/t/p/w300_and_h450_face/vZ9NhNbQQ3yhtiC5sbhpy5KTXns.jpg"
-},
-{
-    "Title": 'Shaun of the Dead',
-    "Description": "(Shaun of the Dead, 2004). Shaun lives a supremely uneventful life, which revolves around his girlfriend, his mother, and, above all, his local pub. This gentle routine is threatened when the dead return to life and make strenuous attempts to snack on ordinary Londoners.",
-    "Genre": {
-        "Name": "Horror Comedy",
-        "Description": "Horror comedy, or comedy Horror, is a genre that combines elements of comedy and horror fiction. Comedy horror has been described as having three types: black comedy, parody and spoof. Comedy horror can also parody or subtly spoof horror clichés as its main source of humour or use those elements to take a story in a different direction."
-    },
-    "Director":{
-        "Name": "Edgar Wright",
-        "Bio": "Edgar Howard Wright is an English filmmaker. He is known for his fast-paced and kinetic, satirical genre films, which feature extensive utilisation of expressive popular music, Steadicam tracking shots, dolly zooms and a signature editing style that includes transitions, whip pans and wipes. In 2004, Wright directed the zombie comedy Shaun of the Dead, starring Pegg and Frost, the first film in Wright's Three Flavours Cornetto trilogy. The film was co-written with Pegg—as were the next two entries in the trilogy, the buddy cop film Hot Fuzz (2007) and the science fiction comedy The World's End (2013). ",
-        "Birthyear": "1974"
-    },
-    "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_face/dgXPhzNJH8HFTBjXPB177yNx6RI.jpg"
-},
-{
-    title: 'Labyrinth'
-},
-{
-    title: 'Constantine'
-},
-{
-    title: 'Princess Mononoke'
-},
-{
-    title: 'Number 6'
-},
-{
-    title: 'Number 7'
-},
-{
-    title: 'Number 8'
-},
-{
-    title: 'Number 9'
-},
-{
-    title: 'Number 10'
-}
-];
-
-let users = [
-    {
-        "Name": "Toni",
-        "Favorites": [],
-        "id": 1
-    },
-    {
-        "Name": "Hans",
-        "Favorites": [],
-        "id": 2
-    }
-];
-
+// log requests to server
 app.use(morgan('common'));
 
 app.use(express.static('public'));
 
-//CREATE = POST
-app.post('/users', (req,res) => {
-    let newUser = req.body;
 
-    if (newUser.Name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send('Please enter a name')
-    }
-});
-
-//UPDATE = PUT
-app.put('/users/:id', (req,res) => {
-    let { id } = req.params;
-    let updatedUser = req.body;
-
-    let user = users.find(user => user.id == id );
-
-    if (user) {
-        user.Name = updatedUser.Name;
-        res.send(`Username successfully updated to ${updatedUser.Name}`)
-    } else {
-        res.status(400).send('No such user found')
-    }
-
-    });
-
-//CREATE = POST
-app.post('/users/:id/:movieTitle', (req,res) => {
-    let { id, movieTitle } = req.params;
-
-    let user = users.find(user => user.id == id );
-
-    if (user) {
-        user.Favorites.push(movieTitle);
-        res.send(`${movieTitle} successfully added to favorites`);
-    } 
-    });
-
-// DELETE = DELETE
-app.delete('/users/:id/:movieTitle', (req,res) => {
-    let { id, movieTitle } = req.params;
-
-    let user = users.find(user => user.id == id );
-
-    if (user) {
-        user.Favorites = user.Favorites.filter( title => title != movieTitle);
-        res.send(`${movieTitle} successfully deleted from favorites`);
-    } 
-
-    });
-
-// DELETE = DELETE
-app.delete('/users/:id', (req,res) => {
-    let { id } = req.params;
-
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        users = users.filter( user => user.id != id);
-        res.send(`The user with id: ${id} has been successfully deleted`);
-    } 
-
-    });
-
-// READ = GET
+// default text response when visiting /
 app.get('/', (req, res) => {
     res.send('Welcome to MovieMento - The movie app!');
 });
 
-app.get('/movies', (req,res) => {
-    res.json(movies);
+// GET JSON object with all movies when visiting /movies
+app.get('/movies', async (req,res) => {
+  await Movies.find()
+    .then((movies) => {
+        res.status(201).json(movies);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});;
+
+// GET (=) JSON with movie Info when looking for specific title
+app.get('/movies/:Title', async (req, res) => {
+    await Movies.findOne({ Title: req.params.Title})
+      .then((movie) => {
+        res.json(movie);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+ });
+
+ // GET JSON with genre Info when looking for specific genre 
+ app.get('/movies/genre/:Name', async (req, res) => {
+    await Movies.find({ 'Genre.Name': req.params.Name  })
+    .then((genre) => {
+        res.json(genre);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+ });
+
+ // GET JSON with info about a director, searching by name
+  app.get('/movies/director/:Name', async (req, res) => {
+    await Movies.find({ 'Director.Name': req.params.Name })
+      .then((director) => {
+         res.json(director);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+ });
+
+// Add a user
+/* We'll expect JSON in this format: 
+{
+ID: Integer (automatically generated),
+Username: String,
+Password: String,
+Email: String,
+Birthday: Date
+} */
+app.post('/users', async (req,res) => {
+    await Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+        if (user) {
+            return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+            Users
+              .create({
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+              })
+              .then((user) => {res.status(201).json(user) })
+            .catch((error) => {
+                console.error(error);
+                res.status(500).send('Error: ' + error);
+            })
+        }
+    })
+    .catch ((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-app.get('/movies/:title', (req, res) => {
-    let title = req.params.title;
-    let movie = movies.find(movie => movie.Title === title);
+// Get all users 
+app.get('/users', async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error ' + err);
+    });
+});
 
-    if (movie) {
-        res.json(movie);
-    } else {
-        res.status(400).send("Sorry, there is no such movie in our list")
-    }  
- });
+// Get a user by username
+app.get('/users/:Username', async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error ' + err);
+    });
+});
 
- app.get('/movies/genre/:genreName', (req, res) => {
-    let genreName = req.params.genreName;
-    let genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+//Update a user's info by username
+    /* We'll expect JSON in this format: 
+    {
+    Username: String, (required)
+    Password: String, (required)
+    Email: String, (required)
+    Birthday: Date
+    } */
+app.put('/users/:Username', async (req,res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {$set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+},
+{ new: true}) // This line makes sure that the updated document is returned
+.then((updatedUser) => {
+  res.json(updatedUser);
+})
+.catch((err) => {
+  console.error(err);
+  res.status(500).send('Error: ' + err);
+  })
+});
 
-    if (genre) {
-        res.json(genre);
-    } else {
-        res.status(400).send("Sorry, there is no such genre in our list")
-    }  
- });
+// Add a movie to a user's list of favorite movies
+app.post('/users/:Username/movies/:MovieID', async (req,res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username}, 
+    { $push: { FavoriteMovies: req.params.MovieID} },
+    { new: true}) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
+});
 
-  app.get('/movies/director/:directorName', (req, res) => {
-    let { directorName } = req.params;
-    let director = movies.find(movie => movie.Director.Name === directorName ).Director;
+// DELETE a movie from a USERS list of favorite movies
+app.delete('/users/:Username/movies/:MovieID', async (req,res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username}, 
+      { $pull: { FavoriteMovies: req.params.MovieID} },
+      { new: true}) // This line makes sure that the updated document is returned
+      .then((updatedUser) => {
+        res.json(updatedUser);
+      })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
+});
 
-    if (director) {
-        res.json(director);
-    } else {
-        res.status(400).send("Sorry, there is no such name in our list")
-    }  
- });
-
-
+// DELETE a user by username
+app.delete('/users/:Username', async (req,res) => {
+    await Users.findOneAndDelete({ Username: req.params.Username})
+      .then((user) => {
+        if (!user) {
+          res.status(400).send(req.params.Username + ' was not found');
+        } else {
+            res.status(200).send(req.params.Username + ' was succsessfull deleted.'); 
+        }
+      })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
